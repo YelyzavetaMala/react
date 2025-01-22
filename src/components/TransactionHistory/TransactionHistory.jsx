ec24ba1056407c827ae8d7038712da7de6398867
YelyzavetaMala <malaelizaveta678@gmail.com>
2025-01-22 22:18:18 +0200

    task 1 3/3

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,5 +1,26 @@
-function TransactionHistory(items) {
-  return <div>{items}</div>;
-}
+import css from "./TransactionHistory.module.css";
+
+const TransactionHistory = ({ items }) => {
+  return (
+    <table className={css.table}>
+      <thead className={css.thead}>
+        <tr>
+          <th className={css.title}>Type</th>
+          <th className={css.title}>Amount</th>
+          <th className={css.title}>Currency</th>
+        </tr>
+      </thead>
+      <tbody>
+        {items.map((item) => (
+          <tr className={css.line} key={item.id}>
+            <td>{item.type}</td>
+            <td>{item.amount}</td>
+            <td>{item.currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
 
 export default TransactionHistory;
