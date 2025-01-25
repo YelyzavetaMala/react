c8db6d3090cdc2bce55d0b896c32dba1cb026046
YelyzavetaMala <malaelizaveta678@gmail.com>
2025-01-25 21:22:12 +0200

    Lesson 2 2/5

--- src/components/MailBox.jsx
+++ src/components/MailBox.jsx
@@ -1,12 +1,27 @@
 function MailBox() {
+  const handleClick = (event) => {
+    console.log(event);
+  };
+  const handleDelete = (mailId) => {
+    console.log("mailId: ", mailId);
+  };
   return (
     <div>
       <h2>MailBox</h2>
       <ul>
-        <li>Mail 1</li>
-        <li>Mail 2</li>
-        <li>Mail 3</li>
+        <li>
+          Mail 1 <button onClick={() => handleDelete(1)}>X</button>
+        </li>
+        <li>
+          Mail 2 <button onClick={() => handleDelete(2)}>X</button>
+        </li>
+        <li>
+          Mail 3 <button onClick={() => handleDelete(3)}>X</button>
+        </li>
       </ul>
+      <button onClick={handleClick} type="button">
+        Send mail
+      </button>
     </div>
   );
 }
