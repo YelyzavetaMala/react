b2fbdee0eaea221485e94fcd8991c021f86105f5
YelyzavetaMala <malaelizaveta678@gmail.com>
2025-01-26 20:00:40 +0200

    Lesson 2 3/4

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,22 @@ import FriendList from "./components/FriendList/FriendList";
 import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
 import transactions from "./transactions.json";
 import MailBox from "./components/MailBox";
+import { useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
   const onLogMail = () => {
     console.log("Mail was sent");
+    setCounter(counter + 1);
   };
   const handleDelete = (mailId) => {
     console.log("mailId: ", mailId);
   };
+
   return (
     <>
+      <h1>Counter: {counter}</h1>
       <div className="css.cardGrid">
         {userData.map((user, id) => (
           <Profile
